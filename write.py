0af0cea46808a1c05904038fe3a49cc848b08c0f
Thomas Allen <tjallen1103@gmail.com>
2021-09-30 19:51:43 -0700

    models.py - done extract.py - done database.py - done filters.py - done write.py - csv write done, json write throws an error
    Signed-off-by: Thomas Allen <tjallen1103@gmail.com>

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+import helpers
 
 
 def write_to_csv(results, filename):
@@ -24,8 +25,21 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km',
+                  'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.DictWriter(csvfile, fieldnames)
+        csvwriter.writeheader()
+        for result in results:
+            csvwriter.writerow({
+                'datetime_utc': result.time,
+                'distance_au': result.distance,
+                'velocity_km_s': result.velocity,
+                'designation': result._designation,
+                'name': result.neo.name,
+                'diameter_km': result.neo.diameter,
+                'potentially_hazardous': result.neo.hazardous})
 
 
 def write_to_json(results, filename):
@@ -40,3 +54,21 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    dict_list = []
+    for result in results:
+        d = {
+                'datetime_utc': helpers.datetime_to_str(result.time),
+                'distance_au': result.distance,
+                'velocity_km_s': result.velocity,
+                "neo": {
+                    'designation': result._designation,
+                    'name': result.neo.name,
+                    'diameter_km': result.neo.diameter,
+                    'potentially_hazardous': result.neo.hazardous,
+                },
+        }
+        dict_list.append(d)
+
+    with open(filename, 'w') as jsonfile:
+        for dictionary in dict_list:
+            json.dump(dictionary, jsonfile)
